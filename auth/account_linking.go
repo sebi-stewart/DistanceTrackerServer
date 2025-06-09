5f364a4fbe2f35a3c82530428df1c9d4336d71ff
Sebi <sebastianstewart03@gmail.com>
2025-06-09 21:06:42 +0100

    Adding logic for the linking of two accounts

--- auth/account_linking.go
+++ auth/account_linking.go
@@ -3,7 +3,10 @@ package auth
 import (
 	"DistanceTrackerServer/models"
 	"DistanceTrackerServer/utils"
+	"database/sql"
+	"errors"
 	"github.com/google/uuid"
+	"time"
 
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -11,24 +14,46 @@ import (
 )
 
 func LinkAccounts(ctx *gin.Context, link models.AccountLink) error {
-	// This function should link the accounts in the database
-	// For example, you might want to update a user record to include the new account ID
-	//dbConn, err := utils.DBConnFromContext(ctx)
-	//if err != nil {
-	//	return err
-	//}
-
-	// Find the corresponding user IDs for the account link by email and password and then UUID
 	// First login to the initiator account and get the user ID
 	initiatorUserID, err := login(ctx, link.Email, link.Password)
 	if err != nil {
 		return err
 	}
 
-	fmt.Print(initiatorUserID)
+	dbConn, err := utils.DBConnFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Now we find the account to link to
+	var linkUserID int
+	var creationTime time.Time
+	err = dbConn.QueryRow("SELECT user_id, created_at FROM link_code WHERE code = ?", link.PairUUID).Scan(&linkUserID, &creationTime)
+	if err != nil {
+		return fmt.Errorf("failed to find link code: %w", err)
+	}
+
+	// Check if the link code is expired (valid for 15 minutes)
+	if time.Since(creationTime) > 15*time.Minute {
+		// If the link code is expired, we delete it from the database
+		_, err = dbConn.Exec("DELETE FROM link_code WHERE code = ?", link.PairUUID)
+		return fmt.Errorf("link code expired")
+	}
+
+	// First we remove any links either account has to any other account
+	_, err = dbConn.Exec("DELETE FROM link_code WHERE user_id = ? OR user_id = ?", initiatorUserID, linkUserID)
+	if err != nil {
+		return fmt.Errorf("failed to remove existing links: %w", err)
+	}
+
+	// Next we link the accounts
+	_, err1 := dbConn.Exec("UPDATE users SET linked_account = ? WHERE id = ?", initiatorUserID, linkUserID)
+	_, err2 := dbConn.Exec("UPDATE users SET linked_account = ? WHERE id = ?", linkUserID, initiatorUserID)
+	if err1 != nil || err2 != nil {
+		return fmt.Errorf("failed to link accounts: %w | %w", err1, err2)
+	}
 
 	return nil
-
 }
 
 func CreateUuidLink(ctx *gin.Context, origin models.UserLogin) (models.AccountLink, error) {
@@ -44,10 +69,31 @@ func CreateUuidLink(ctx *gin.Context, origin models.UserLogin) (models.AccountLi
 		return models.AccountLink{}, err
 	}
 
+	// Delete any existing link code for the initiator user
 	_, err = dbConn.Exec("DELETE FROM link_code WHERE user_id = ?", initiatorUserID)
 	if err != nil {
 		return models.AccountLink{}, fmt.Errorf("failed to delete existing link code: %w", err)
 	}
+
+	// Remove any existing linked accounts for the initiator user, or the potential link user
+	// This ensures that the initiator user can only have one link code at a time
+	var existingLinkUserID int
+	err = dbConn.QueryRow("SELECT linked_account FROM users WHERE id = ?", initiatorUserID).Scan(&existingLinkUserID)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return models.AccountLink{}, fmt.Errorf("failed to get existing linked account: %w", err)
+	}
+
+	if existingLinkUserID != 0 {
+		_, err = dbConn.Exec("UPDATE users SET linked_account = NULL WHERE id = ?", existingLinkUserID)
+		if err != nil {
+			return models.AccountLink{}, fmt.Errorf("failed to remove existing linked account: %w", err)
+		}
+	}
+	_, err = dbConn.Exec("UPDATE users SET linked_account = NULL WHERE id = ?", initiatorUserID)
+	if err != nil {
+		return models.AccountLink{}, fmt.Errorf("failed to remove existing linked account: %w", err)
+	}
+
 	_, err = dbConn.Exec("INSERT INTO link_code (user_id, code) VALUES (?, ?)", initiatorUserID, pairUUID)
 	if err != nil {
 		return models.AccountLink{}, fmt.Errorf("failed to insert new link code: %w", err)
